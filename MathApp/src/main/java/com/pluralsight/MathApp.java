79429f33f5474713e5296f73fe28008896df82c5
dave <dave@brightworkdigital.com>
2025-04-10 17:57:16 +0100

    initial MathApp commit

---
+++ MathApp/src/main/java/com/pluralsight/MathApp.java
@@ -0,0 +1,20 @@
+package com.pluralsight;
+
+public class MathApp {
+
+    public static void main(String[] args) {
+        // Question 1:
+        // declare variables here
+        double bobSalary = 50000;
+        double garySalary = 200000.99;
+
+        // then code solution
+        double highestSalary = Math.max(bobSalary, garySalary);
+
+        // Print out result
+        System.out.println("Highest salary is: " + highestSalary);
+
+        // Question 2:
+
+    }
+}
